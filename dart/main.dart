3a6975d4bd034f3eb6ddb17139fa2e3ac8a98329
blackwolf951 <153739402+blackwolf951@users.noreply.github.com>
2024-10-04 05:21:34 +0800

    Update main.dart

--- dart/main.dart
+++ dart/main.dart
@@ -4,12 +4,12 @@ import 'pass_io.dart'; // 引入登入界面
 import 'mqtt_api.dart'; // 引入 MQTT 和 API
 
 void main() {
-  // 初始化数据库工厂用于桌面环境
+  // 初始化資料庫和主畫面
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
 
   runApp(MyApp());
-  startApiServer(); // 启动 API 服务器
+  startApiServer(); // 啟動 API 伺服器
 }
 
 class MyApp extends StatelessWidget {
